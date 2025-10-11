fa462d19c5fd0fba0743801d1c46783328ea1e48
qualcunu <72537184+qualcunu@users.noreply.github.com>
2025-10-11 08:53:40 -0400

    Update TitleCard.tsx

--- apps/client/src/common/components/title-card/TitleCard.tsx
+++ apps/client/src/common/components/title-card/TitleCard.tsx
@@ -10,22 +10,21 @@ interface TitleCardProps {
   label?: 'now' | 'next';
   secondary?: string;
   className?: string;
-  stayl?: string;
 }
 
 const TitleCard = forwardRef((props: TitleCardProps, ref: ForwardedRef<HTMLDivElement>) => {
-  const { label, title, secondary, stayl, className = '' } = props;
+  const { label, title, secondary, className = '' } = props;
   const { getLocalizedString } = useTranslation();
 
   const accent = label === 'now';
 
   return (
     <div className={cx(['title-card', className])} ref={ref}>
-      <span className='title-card__title' style={stayl}>{title}</span>
+      <span className='title-card__title' style={{color: secondary}}>{title}</span>
       <span className={cx(['title-card__label', accent && 'title-card__label--accent'])}>
         {label && getLocalizedString(`common.${label}`)}
       </span>
-      <div className='title-card__secondary' style={{color: stayl }}>{secondary}</div>
+      <div className='title-card__secondary' style={{color: secondary }}>{secondary}</div>
     </div>
   );
 });
