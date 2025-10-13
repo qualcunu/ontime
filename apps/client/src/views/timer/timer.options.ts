4a7361c0df625fe80b3545fcc80648f9d21bef5c
qualcunu <72537184+qualcunu@users.noreply.github.com>
2025-10-13 11:47:43 -0400

    Update timer.options.ts

--- apps/client/src/views/timer/timer.options.ts
+++ apps/client/src/views/timer/timer.options.ts
@@ -1,248 +1,188 @@
-import { use, useMemo } from 'react';
-import { useSearchParams } from 'react-router';
-import { CustomFields, OntimeEvent, TimerType } from 'ontime-types';
-import { validateTimerType } from 'ontime-utils';
+import { MaybeNumber, MessageState, OntimeEvent, Playback, TimerMessage, TimerPhase, TimerType } from 'ontime-types';
+import { isPlaybackActive } from 'ontime-utils';
 
-import type { SelectOption } from '../../common/components/select/Select';
-import {
-  getTimeOption,
-  hideTimerSeconds,
-  showLeadingZeros,
-} from '../../common/components/view-params-editor/common.options';
-import { OptionTitle } from '../../common/components/view-params-editor/constants';
-import { ViewOption } from '../../common/components/view-params-editor/viewParams.types';
-import { makeOptionsFromCustomFields } from '../../common/components/view-params-editor/viewParams.utils';
-import { PresetContext } from '../../common/context/PresetContext';
-import { isStringBoolean, makeColourString } from '../../features/viewers/common/viewUtils';
+import { getFormattedTimer, getPropertyValue } from '../../features/viewers/common/viewUtils';
 
-// manually match the properties of TimerType excluding the None
-const timerDisplayOptions: SelectOption[] = [
-  { value: 'no-overrides', label: 'No Overrides' },
-  { value: TimerType.CountUp, label: 'Count Up' },
-  { value: TimerType.CountDown, label: 'Count Down' },
-  { value: TimerType.Clock, label: 'Clock' },
-];
+/**
+ * Whether a message should be shown
+ */
+export function getShowMessage(message: TimerMessage): boolean {
+  return message.text !== '' && message.visible;
+}
 
-export const getTimerOptions = (timeFormat: string, customFields: CustomFields): ViewOption[] => {
-  const mainOptions = makeOptionsFromCustomFields(customFields, [
-    { value: 'none', label: 'None' },
-    { value: 'title', label: 'Title' },
-    { value: 'note', label: 'Note' },
-  ]);
-  const secondaryOptions = makeOptionsFromCustomFields(customFields, [
-    { value: 'none', label: 'None' },
-    { value: 'title', label: 'Title' },
-    { value: 'note', label: 'Note' },
-  ]);
+/**
+ * Whether the playback is playing
+ */
+export function getIsPlaying(playback: Playback): boolean {
+  return playback === Playback.Play || playback === Playback.Roll;
+}
 
-  return [
-    { title: OptionTitle.ClockOptions, collapsible: true, options: [getTimeOption(timeFormat)] },
-    {
-      title: OptionTitle.TimerOptions,
-      collapsible: true,
-      options: [
-        hideTimerSeconds,
-        showLeadingZeros,
-        {
-          id: 'timerType',
-          title: 'Timer type',
-          description: 'Override the timer type',
-          type: 'option',
-          values: timerDisplayOptions,
-          defaultValue: 'no-overrides',
-        },
-        {
-          id: 'freezeOvertime',
-          title: 'Freeze Overtime',
-          description: 'If active, the timer will not count into negative numbers',
-          type: 'boolean',
-          defaultValue: false,
-        },
-        {
-          id: 'freezeMessage',
-          title: 'Freeze Message',
-          description:
-            'An optional message to show when the timer is in overtime (must be set in combination with Freeze Overtime)',
-          type: 'string',
-          defaultValue: '',
-          placeholder: 'e.g. Time is up!',
-        },
-        {
-          id: 'hidePhase',
-          title: 'Hide progress styles',
-          description: 'Whether to suppress the progress styles (warning, danger and overtime)',
-          type: 'boolean',
-          defaultValue: false,
-        },
-      ],
-    },
-    {
-      title: OptionTitle.DataSources,
-      collapsible: true,
-      options: [
-        {
-          id: 'main',
-          title: 'Main text',
-          description: 'Select the data source for the main text',
-          type: 'option',
-          values: mainOptions,
-          defaultValue: 'title',
-        },
-        {
-          id: 'secondary-src',
-          title: 'Secondary text',
-          description: 'Select the data source for the secondary text',
-          type: 'option',
-          values: secondaryOptions,
-          defaultValue: 'none',
-        },
-      ],
-    },
-    {
-      title: OptionTitle.ElementVisibility,
-      collapsible: true,
-      options: [
-        {
-          id: 'hideClock',
-          title: 'Hide Time Now',
-          description: 'Hides the Time Now field',
-          type: 'boolean',
-          defaultValue: false,
-        },
-        {
-          id: 'hideCards',
-          title: 'Hide Cards',
-          description: 'Hides the Now and Next cards',
-          type: 'boolean',
-          defaultValue: false,
-        },
-        {
-          id: 'hideProgress',
-          title: 'Hide progress bar',
-          description: 'Hides the progress bar',
-          type: 'boolean',
-          defaultValue: false,
-        },
-        {
-          id: 'hideMessage',
-          title: 'Hide Timer Message',
-          description: 'Prevents displaying fullscreen messages in the timer',
-          type: 'boolean',
-          defaultValue: false,
-        },
-        {
-          id: 'hideSecondary',
-          title: 'Hide Auxiliary timer / Secondary message',
-          description: 'Prevents the screen from displaying the secondary timer field',
-          type: 'boolean',
-          defaultValue: false,
-        },
-        {
-          id: 'hideLogo',
-          title: 'Hide the project logo',
-          description: 'Prevents the screen from displaying the given project logo',
-          type: 'boolean',
-          defaultValue: false,
-        },
-      ],
-    },
-    {
-      title: OptionTitle.StyleOverride,
-      collapsible: true,
-      options: [
-        {
-          id: 'font',
-          title: 'Font',
-          description: 'Font family, will use the fonts available in the system',
-          type: 'string',
-          placeholder: 'Open Sans (default)',
-        },
-        {
-          id: 'keyColour',
-          title: 'Key Colour',
-          description: 'Background or key colour for entire view. Default: #101010',
-          type: 'colour',
-          defaultValue: '101010',
-        },
-        {
-          id: 'timerColour',
-          title: 'Timer Colour',
-          description: 'Timer colour. Default: #f6f6f6',
-          type: 'colour',
-          defaultValue: 'f6f6f6',
-        },
-      ],
-    },
-  ];
-};
+/**
+ * Gets the total time from the duration and added time of an event
+ */
+export function getTotalTime(duration: MaybeNumber, addedTime: MaybeNumber): number {
+  return (duration ?? 0) + (addedTime ?? 0);
+}
 
-type TimerOptions = {
-  hideClock: boolean;
-  hideCards: boolean;
-  hideProgress: boolean;
-  hideMessage: boolean;
-  hideSecondary: boolean;
-  hideLogo: boolean;
-  hideTimerSeconds: boolean;
-  removeLeadingZeros: boolean;
-  mainSource: keyof OntimeEvent | null;
-  secondarySource: keyof OntimeEvent | null;
-  timerType?: TimerType;
-  freezeOvertime: boolean;
-  freezeMessage: string;
-  hidePhase: boolean;
-  font?: string;
-  keyColour?: string;
-  timerColour?: string;
+/**
+ * Whether the progress bar should be shown for this timer type
+ */
+export function getShowProgressBar(timerType: TimerType) {
+  return timerType !== TimerType.None && timerType !== TimerType.Clock;
+}
+
+/**
+ * Whether the clock should be shown with this timer type
+ */
+export function getShowClock(timerType: TimerType) {
+  return timerType !== TimerType.Clock;
+}
+
+const fontSizeMap: { [key: number]: number } = {
+  4: 28, // 9:01
+  5: 28, // -9:01, 10:01, 9 min
+  6: 25, // -10:01, 10 min
+  8: 18, // 23:01:01
+  9: 18, // -23:01:01
 };
 
 /**
- * Utility extract the view options from URL Params
- * the names and fallbacks are manually matched with timerOptions
+ * Finds a font size that fits the timer in the screen
+ * Unfortunately hand tweaked
  */
-function getOptionsFromParams(searchParams: URLSearchParams, defaultValues?: URLSearchParams): TimerOptions {
-  // Helper to get value from either source, prioritizing defaultValues
-  const getValue = (key: string) => defaultValues?.get(key) ?? searchParams.get(key);
+export function getEstimatedFontSize(stageTimer: string, secondaryContent?: string) {
+  const stageTimerCharacters = stageTimer.length;
+  let timerFontSize = (100 / (stageTimerCharacters - 1)) * 1.25;
 
-  // Get timerType from either source
-  const timerType = validateTimerType(getValue('timerType'), TimerType.None);
+  if (fontSizeMap[stageTimerCharacters]) {
+    timerFontSize = fontSizeMap[stageTimerCharacters];
+  }
+
+  let externalFontSize = timerFontSize * 0.325;
+  if (secondaryContent) {
+    // we need to shrink the timer if the external is going to be there
+    // this number has been tweaked to fit in a landscape mobile screen
+    timerFontSize *= 0.6;
+    if (secondaryContent.length > 25) {
+      externalFontSize = (100 / (secondaryContent.length - 1)) * 1.8;
+    }
+  }
 
   return {
-    hideClock: isStringBoolean(getValue('hideClock')),
-    hideCards: isStringBoolean(getValue('hideCards')),
-    hideProgress: isStringBoolean(getValue('hideProgress')),
-    hideMessage: isStringBoolean(getValue('hideMessage')),
-    hideSecondary: isStringBoolean(getValue('hideSecondary')),
-    hideLogo: isStringBoolean(getValue('hideLogo')),
-    hideTimerSeconds: isStringBoolean(getValue('hideTimerSeconds')),
-    removeLeadingZeros: !isStringBoolean(getValue('showLeadingZeros')),
-
-    mainSource: getValue('main') as keyof OntimeEvent | null,
-    secondarySource: getValue('secondary-src') as keyof OntimeEvent | null,
-
-    // none doesnt make sense as a configuration of the view
-    timerType: timerType === TimerType.None ? undefined : timerType,
-    freezeOvertime: isStringBoolean(getValue('freezeOvertime')),
-    freezeMessage: getValue('freezeMessage') ?? '',
-    hidePhase: isStringBoolean(getValue('hidePhase')),
-
-    font: getValue('font') ?? undefined,
-    keyColour: makeColourString(getValue('keyColour')),
-    timerColour: makeColourString(getValue('timerColour')),
+    timerFontSize,
+    externalFontSize,
   };
 }
 
 /**
- * Hook exposes the timer view options
+ * which, if any, modifier should be shown at any time
  */
-export function useTimerOptions(): TimerOptions {
-  const [searchParams] = useSearchParams();
-  const maybePreset = use(PresetContext);
+export function getShowModifiers(
+  timerType: TimerType,
+  countToEnd: boolean,
+  phase: TimerPhase,
+  freezeOvertime: boolean,
+  freezeMessage: string,
+  hidePhase: boolean,
+) {
+  if (hidePhase) {
+    return {
+      showEndMessage: false,
+      showFinished: false,
+      showWarning: false,
+      showDanger: false,
+    };
+  }
 
-  const options = useMemo(() => {
-    const defaultValues = maybePreset ? new URLSearchParams(maybePreset.search) : undefined;
-    return getOptionsFromParams(searchParams, defaultValues);
-  }, [maybePreset, searchParams]);
+  const showModifiers = timerType === TimerType.CountDown || countToEnd;
+  if (!showModifiers) {
+    return {
+      showEndMessage: false,
+      showFinished: false,
+      showWarning: false,
+      showDanger: false,
+    };
+  }
 
-  return options;
+  return {
+    showEndMessage: freezeOvertime && freezeMessage !== '',
+    showFinished: phase === TimerPhase.Overtime,
+    showWarning: phase === TimerPhase.Warning,
+    showDanger: phase === TimerPhase.Danger,
+  };
+}
+
+/**
+ * What, if anything, should be displayed in the secondary field
+ */
+export function getSecondaryDisplay(
+  message: MessageState,
+  currentAux: MaybeNumber,
+  localisedMinutes: string,
+  removeSeconds: boolean,
+  removeLeadingZero: boolean,
+  hideSecondary: boolean,
+): string | undefined {
+  if (hideSecondary) {
+    return;
+  }
+  if (
+    message.timer.secondarySource === 'aux1' ||
+    message.timer.secondarySource === 'aux2' ||
+    message.timer.secondarySource === 'aux3'
+  ) {
+    return getFormattedTimer(currentAux, TimerType.CountDown, localisedMinutes, {
+      removeSeconds,
+      removeLeadingZero,
+    });
+  }
+  if (message.timer.secondarySource === 'secondary' && message.secondary) {
+    return message.secondary;
+  }
+  return;
+}
+
+/**
+ * What should we be showing in the cards?
+ */
+export function getCardData(
+  eventNow: OntimeEvent | null,
+  eventNext: OntimeEvent | null,
+  mainSource: keyof OntimeEvent | null,
+  secondarySource: keyof OntimeEvent | null,
+  playback: Playback,
+  phase: TimerPhase,
+) {
+  if (playback === Playback.Stop) {
+    return {
+      showNow: false,
+      nowMain: undefined,
+      nowSecondary: undefined,
+      showNext: false,
+      nextMain: undefined,
+      nextSecondary: undefined,
+    };
+  }
+
+  // pending roll timers would be classified as active
+  const hasActiveTimer = isPlaybackActive(playback) && phase !== TimerPhase.Pending;
+
+  // if we are loaded, we show the upcoming event as next
+  const nowMain = hasActiveTimer ? getPropertyValue(eventNow, mainSource ?? 'title') : undefined;
+  const nowSecondary = hasActiveTimer ? getPropertyValue(eventNow, secondarySource) : undefined;
+  const nextMain = hasActiveTimer
+    ? getPropertyValue(eventNext, mainSource ?? 'title')
+    : getPropertyValue(eventNow, mainSource ?? 'title');
+  const nextSecondary = hasActiveTimer
+    ? getPropertyValue(eventNext, secondarySource)
+    : getPropertyValue(eventNow, secondarySource);
+
+  return {
+    showNow: Boolean(nowMain) || Boolean(nowSecondary),
+    nowMain,
+    nowSecondary,
+    showNext: Boolean(nextMain) || Boolean(nextSecondary),
+    nextMain,
+    nextSecondary,
+  };
 }
