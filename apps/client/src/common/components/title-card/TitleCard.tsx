18cc3ab9cb4b308c416ac1c5dd918cb61f6e2dfc
qualcunu <72537184+qualcunu@users.noreply.github.com>
2025-10-13 09:54:47 -0400

    Update TitleCard.tsx

--- apps/client/src/common/components/title-card/TitleCard.tsx
+++ apps/client/src/common/components/title-card/TitleCard.tsx
@@ -9,24 +9,26 @@ interface TitleCardProps {
   title?: string;
   label?: 'now' | 'next';
   secondary?: string;
+  color?: string;
+  bgColor?: string;
   className?: string;
-  lel?: string;
-  lawl?: string;
 }
 
 const TitleCard = forwardRef((props: TitleCardProps, ref: ForwardedRef<HTMLDivElement>) => {
-  const { label, title, secondary, className = '', lel, lawl} = props;
+  const { label, title, secondary, color, bgColor, className = '', } = props;
   const { getLocalizedString } = useTranslation();
 
   const accent = label === 'now';
+  console.log("It updated successfully again.")
+  console.log(props)
 
   return (
-    <div className={cx(['title-card', className])} style={{backgroundColor: lawl}} ref={ref}>
-      <span className='title-card__title' style={{color: lel}}>{title}</span>
-      <span className={cx(['title-card__label', accent && 'title-card__label--accent'])} style={{color: lel}}>
+    <div className={cx(['title-card', className])} style={{backgroundColor: bgColor}} ref={ref}>
+      <span className='title-card__title' style={{color: color}}>{title}</span>
+      <span className={cx(['title-card__label', accent && 'title-card__label--accent'])} style={{color: color}}>
         {label && getLocalizedString(`common.${label}`)}
-      </span>
-      <div className='title-card__secondary' style={{color: lel}}>{secondary}</div>
+              </span>
+      <div className='title-card__secondary' style={{color: color}}>{secondary}</div>
     </div>
   );
 });
