121df0a5917f2ff0f4e388533d04d229b6f80b33
qualcunu <72537184+qualcunu@users.noreply.github.com>
2025-10-18 18:00:58 -0400

    Update ProjectInfo.tsx

--- apps/client/src/views/project-info/ProjectInfo.tsx
+++ apps/client/src/views/project-info/ProjectInfo.tsx
@@ -1,100 +1,66 @@
-import { IoOpenOutline } from 'react-icons/io5';
-import { OntimeView } from 'ontime-types';
+import { useRef } from 'react';
+import { useDisclosure } from '@mantine/hooks';
 
-import EmptyPage from '../../common/components/state/EmptyPage';
-import ViewLogo from '../../common/components/view-logo/ViewLogo';
-import ViewParamsEditor from '../../common/components/view-params-editor/ViewParamsEditor';
-import { useWindowTitle } from '../../common/hooks/useWindowTitle';
-import { useTranslation } from '../../translation/TranslationProvider';
-import Loader from '../common/loader/Loader';
+import Button from '../../common/components/buttons/Button';
+import Dialog from '../../common/components/dialog/Dialog';
+import { useElectronEvent } from '../../common/hooks/useElectronEvent';
+import { isLocalhost, isOntimeCloud } from '../../externals';
+import * as Panel from '../../features/app-settings/panel-utils/PanelUtils';
 
-import { ProjectInfoData, useProjectInfoData } from './useProjectInfoData';
+export default function ShutdownPanel() {
+  const { isElectron, sendToElectron } = useElectronEvent();
+  const [isOpen, handler] = useDisclosure();
+  const cancelRef = useRef<HTMLButtonElement | null>(null);
 
-import './ProjectInfo.scss';
+  const sendShutdown = () => {
+    sendToElectron('shutdown', 'now');
+    handler.close();
+  };
 
-export default function ProjectInfoLoader() {
-  const { data, status } = useProjectInfoData();
-
-  useWindowTitle('Project info');
-
-  if (status === 'pending') {
-    return <Loader />;
-  }
-
-  if (status === 'error') {
-    return <EmptyPage text='There was an error fetching data, please refresh the page.' />;
-  }
-
-  return <ProjectInfo {...data} />;
-}
-
-function ProjectInfo({ projectData, isMirrored }: ProjectInfoData) {
-  const { getLocalizedString } = useTranslation();
-
-  /**
-   * Check if there is data to show at all
-   * We need a special check for the project fields which can be an empty array
-   */
-  const isEmpty =
-    !projectData ||
-    Object.values(projectData).every((value) => !value || (value && Array.isArray(value) && value.length === 0));
-  if (isEmpty) {
-    return (
-      <>
-        <ViewParamsEditor target={OntimeView.ProjectInfo} viewOptions={[]} />
-        <EmptyPage text={getLocalizedString('common.no_data')} />;
-      </>
-    );
-  }
+  const canShutdown = isElectron || isLocalhost;
 
   return (
-    <div className={`project ${isMirrored ? 'mirror' : ''}`} data-testid='project-view'>
-      <ViewParamsEditor target={OntimeView.ProjectInfo} viewOptions={[]} />
-      {projectData.logo && <ViewLogo name={projectData.logo} className='logo' />}
-      <div className='info'>
-        {projectData.title && (
-          <div>
-            <div className='info__label'>{getLocalizedString('project.title')}</div>
-            <div className='info__value'>{projectData.title}</div>
-          </div>
+    <>
+      <Panel.Header>Shutdown Ontime</Panel.Header>
+      <Panel.Section>
+        {isOntimeCloud ? (
+          <Panel.Highlight>
+            For security reasons, shutting down the server must be done from the Ontime Cloud dashboard.
+          </Panel.Highlight>
+        ) : (
+          <Panel.Paragraph>
+            This will shutdown the Ontime server. <br />
+            The runtime state will be lost, but your project is kept for next time.
+          </Panel.Paragraph>
         )}
-        {projectData.description && (
-          <div>
-            <div className='info__label'>{getLocalizedString('project.description')}</div>
-            <div className='info__value'>{projectData.description}</div>
-          </div>
+        <Button variant='destructive' onClick={handler.open} disabled={!(isElectron || isLocalhost)}>
+          Shutdown ontime
+        </Button>
+        {!canShutdown && (
+          <Panel.Description>Note: Ontime can only be shutdown from the machine it is running in.</Panel.Description>
         )}
-        {projectData.info && (
-          <div>
-            <div className='info__label'>{getLocalizedString('project.info')}</div>
-            <div className='info__value'>{projectData.info}</div>
-          </div>
-        )}
-        {projectData.url && (
-          <div>
-            <div className='info__label'>{getLocalizedString('project.url')}</div>
-            <a href={projectData.url} target='_blank' rel='noreferrer' className='info__value link'>
-              {projectData.url} <IoOpenOutline style={{ fontSize: '1em' }} />
-            </a>
-          </div>
-        )}
-        {projectData.custom.map((info, idx) => {
-          const hasUrl = Boolean(info.url);
-          return (
-            <div key={`${info.title}-${idx}`} className='info__custom'>
-              {hasUrl && (
-                <div className='info__image-container'>
-                  <img className='info__image' src={info.url} loading='lazy' />
-                </div>
-              )}
-              <div>
-                <div className='info__label'>{info.title}</div>
-                <div className='info__value'>{info.value}</div>
-              </div>
-            </div>
-          );
-        })}
-      </div>
-    </div>
+        <Dialog
+          isOpen={isOpen}
+          title='Shutdown Ontime'
+          showCloseButton
+          onClose={handler.close}
+          bodyElements={
+            <Panel.Paragraph>
+              This will shutdown the Ontime server. <br /> Are you sure?
+            </Panel.Paragraph>
+          }
+          footerElements={
+            <>
+              <Button ref={cancelRef} onClick={handler.close} variant='ghosted-white'>
+                Cancel
+              </Button>
+              <Button variant='destructive' onClick={sendShutdown} disabled={!canShutdown}>
+                Shutdown
+              </Button>
+            </>
+          }
+        />
+      </Panel.Section>
+    </>
   );
 }
